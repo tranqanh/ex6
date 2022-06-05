fbbae5a42efd420d5861100f79fb3bef14a440f5
quanganh <tranquanganhvnb@gmail.com>
2022-06-05 17:13:27 +0700

    Lab 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
-            $(document).ready(function() {
-                $('[data-toggle="tooltip"]').tooltip();
-            });
+$("#carouselButton").click(function() {
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');
+    }
+});
 
-            $("#carouselButton").click(function(){
-                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-                    $("#mycarousel").carousel('pause');
-                    $("#carouselButton").children("span").removeClass('fa-pause');
-                    $("#carouselButton").children("span").addClass('fa-play');
-                }
-                else if ($("#carouselButton").children("span").hasClass('fa-play')){
-                    $("#mycarousel").carousel('cycle');
-                    $("#carouselButton").children("span").removeClass('fa-play');
-                    $("#carouselButton").children("span").addClass('fa-pause');                    
-                }
-            });
+$("#reverve-button").click(function() {
+    $("#reserveForm").modal();
+});
+
+$("#signin-button").click(function() {
+    $("#loginModal").modal();
+});
